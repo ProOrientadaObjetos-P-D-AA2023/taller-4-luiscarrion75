9f856d85c03030c6768ea51d7841f4403f338ceb
luiscarrion75 <116389732+luiscarrion75@users.noreply.github.com>
2023-05-06 13:01:46 -0500

    Problema 3

---
+++ problema3/caracteristicasAuto.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caracteristicas;
+
+/**
+ *
+ * @author DELL
+ */
+public class caracteristicasAuto {
+    private String cedula;
+    private int añof;
+    private double vv;
+    private double vm;
+    private fabricante fabri;
+    double añosantiguedad;
+    double vporciento;
+    double porci;
+    public caracteristicasAuto(String cedu, int añofa, double vave, fabricante fabric) {
+        cedula = cedu;
+        añof = añofa;
+        vv = vave;
+        vm = vm;
+        fabri = fabric;
+    }
+    
+}
---
+++ problema3/fabricante.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caracteristicas;
+
+/**
+ *
+ * @author DELL
+ */
+public class fabricante {
+    private String nfabricante;
+    private String ciuorigen;
+
+    public fabricante(String nofabricante, String ciudorigen) {
+        nfabricante = nofabricante;
+        ciuorigen = ciudorigen;
+    }
+
+    
+}
