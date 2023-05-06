8432c51c708e7d9f513db173b8034e3e0f88edcd
luiscarrion75 <116389732+luiscarrion75@users.noreply.github.com>
2023-05-06 01:57:54 -0500

    Problema 2

---
+++ problema2/DatosProfesor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Datos;
+
+/**
+ *
+ * @author DELL
+ */
+public class DatosProfesor {
+    private String nombre;
+    private String apellido;
+    private double sb;
+    private double st;
+    private String cedula;
+    private Provincia pro;
+    double porcientosb;
+
+    public DatosProfesor(String nom, String ape, double suba, String ced, Provincia prov) {
+        nombre = nom;
+        apellido = ape;
+        sb = suba;
+        st = st;
+        cedula = ced;
+        pro = prov;
+        
+        
+    }
+}
---
+++ problema2/EjecutarFichaProfesor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejecutarfichaprofesor;
+
+/**
+ *
+ * @author DELL
+ */
+public class EjecutarFichaProfesor {
+
+    public static void main(String[] args) {
+        System.out.println("Ficha Profesor");
+        
+        
+        
+        
+    }
+}
